79a66c3720baf2bd11a83fff064e5fd408bb9caa
GIT Asagishi <asagishi.ad@gmail.com>
2018-12-05 11:41:48 +0700

    Update sendit.cloud.php

--- hosts/upload/sendit.cloud.php
+++ hosts/upload/sendit.cloud.php
@@ -19,8 +19,8 @@ $_T8['anonUploadLimit'] = 0; // File-size limit for non-registered users (MB) | 
 $acc_key_name = str_ireplace(array('www.', '.'), array('', '_'), $_T8['domain']); // (Do Not Edit)
 
 /* # Account Info # */
-$upload_acc[$acc_key_name]['user'] = ''; //Set your login
-$upload_acc[$acc_key_name]['pass'] = ''; //Set your password
+$upload_acc[$acc_key_name]['user'] = 'asagishi25'; //Set your login
+$upload_acc[$acc_key_name]['pass'] = 'qwerty123@'; //Set your password
 
 if (!file_exists(HOST_DIR . 'upload/GenericXFSHost.inc.php')) html_error('Cannot load "'.htmlentities(HOST_DIR).'upload/GenericXFSHost.inc.php" (File doesn\'t exists), please install lastest version from: http://www.rapidleech.com/index.php/topic/14014-upload-plugin-for-sites-with-xfs-pro/ or http://pastebin.com/E0z7qMU1 ');
 require(HOST_DIR . 'upload/GenericXFSHost.inc.php');
