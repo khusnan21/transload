19f8c51a88e561fd5f8ea9751b9f7f50fef114d5
GIT Asagishi <asagishi.ad@gmail.com>
2018-12-27 10:24:05 +0700

    Update apimal.php

--- apimal.php
+++ apimal.php
@@ -6,7 +6,7 @@ if(isset($_GET["url"])){
 	if($_GET["url"] != ""){
 		$MyAnimeListAPI = new MyAnimeListAPI();
 		$urlin = "https://myanimelist.net/anime/".$_GET["url"]."/Captain_Tsubasa_2018";
-		$MAL_Data       = $MyAnimeListAPI->get_data($_GET["url"]);
+		$MAL_Data       = $MyAnimeListAPI->get_data($urlin);
 		echo json_encode($MAL_Data);
 	}
 }
