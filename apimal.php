5f28db7cb2f9726370eb0566037f2b7b777760c6
GIT Asagishi <asagishi.ad@gmail.com>
2018-12-27 10:20:06 +0700

    Update apimal.php

--- apimal.php
+++ apimal.php
@@ -5,6 +5,7 @@ header("Access-Control-Allow-Origin: *");
 if(isset($_GET["url"])){
 	if($_GET["url"] != ""){
 		$MyAnimeListAPI = new MyAnimeListAPI();
+		$urlin = "https://myanimelist.net/anime/".$_GET["url"]."/Captain_Tsubasa_2018";
 		$MAL_Data       = $MyAnimeListAPI->get_data($_GET["url"]);
 		echo json_encode($MAL_Data);
 	}
